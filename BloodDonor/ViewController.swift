b3540f21380646d14bea6b55fcdfcaa57c3352a4
shokhsanam <sanamxon1994@gmail.com>
2017-09-19 15:17:47 -0700

    Initial Commit

---
+++ BloodDonor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BloodDonor
+//
+//  Created by shokhsanam on 9/19/17.
+//  Copyright © 2017 Inha University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
